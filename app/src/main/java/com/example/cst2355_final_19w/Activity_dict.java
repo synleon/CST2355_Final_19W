73c575e2fd138f6fb6396d180f6d1075a42ef12c
synleon <synleon@gmail.com>
2019-03-19 08:31:54 -0400

    Add toast message

--- app/src/main/java/com/example/cst2355_final_19w/Activity_dict.java
+++ app/src/main/java/com/example/cst2355_final_19w/Activity_dict.java
@@ -2,6 +2,7 @@ package com.example.cst2355_final_19w;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Activity_dict extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class Activity_dict extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dict);
+
+        Toast.makeText(this, "You have entered dictionary page!", Toast.LENGTH_LONG).show();
     }
 }
