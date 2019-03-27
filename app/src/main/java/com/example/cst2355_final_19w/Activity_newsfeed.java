9f90013eb8cb56c4da54ae5d21f29e4b7fc1cecd
synleon <synleon@gmail.com>
2019-03-27 00:06:57 -0400

    dictionary activity milestone1 merged into master

---
+++ app/src/main/java/com/example/cst2355_final_19w/Activity_newsfeed.java
@@ -0,0 +1,166 @@
+package com.example.cst2355_final_19w;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.support.design.widget.Snackbar;
+
+public class Activity_newsfeed extends AppCompatActivity
+{
+    Toolbar tBar;
+
+    // need to be changed
+    public String[] news = {"news1", "news2","new3", "new4","new5","news6", "news7","new8", "new9","new10",
+            "news11", "news12","new13", "new14","new15"};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newsfeed);
+
+        // create an object of progress bar and set it visible.
+        ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        // create an object of tool bar and display it.
+        tBar = (Toolbar)findViewById(R.id.toolbar_newsF);
+        setSupportActionBar(tBar);
+
+        // set function for "GO BACK" button
+        Button goBackBu = (Button)findViewById(R.id.goback_newsF);
+        goBackBu.setOnClickListener( b -> {
+            Intent goBackIntent = new Intent(Activity_newsfeed.this, MainActivity.class);
+            startActivity(goBackIntent);
+        });
+
+        // create an object of listView
+        ListView newsList = (ListView) findViewById(R.id.list_newsF);
+        NewsAdapter adapter = new NewsAdapter();
+        newsList.setAdapter(adapter);
+
+        LayoutInflater inflater = getLayoutInflater();
+        //View newView = inflater.inflate(R.layout.activity_list_newsfeed,parent,false);
+    }
+
+    // display the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_newsfeed, menu);
+
+        return true;
+    }
+
+    // define the function for each item in the toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case R.id.dictionary:
+                // make toast when click dictionary icon on toolbar
+                Toast.makeText(this, "You have come to Dictionary Section.", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.flightStat_newsFeed:
+                // make toast when click flight icon on toolbar
+                Toast.makeText(this, "You have come to Flight Status Section.", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item_NYtime_newsFeed:
+                // show a snack bar when click New York Time icon on toolbar
+                showSnackBar();
+                break;
+            case R.id.item_help_newsFeed:
+                // make a alert when click overflow title
+                alert();
+                break;
+        }
+        return true;
+    }
+
+    // this method will be executed when the item "HELP" in the toolbar is clicked.
+    public void alert()
+    {
+        View helpView = getLayoutInflater().inflate(R.layout.activity_dialogbox_newsfeed, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.help_newsF)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // What to do on Accept
+                    }
+                }).setView(helpView);
+
+        builder.create().show();
+    }
+
+    // this method will be executed when the item "New York Times" in the toolbar is clicked.
+    public void showSnackBar()
+    {
+        tBar = (Toolbar)findViewById(R.id.toolbar_newsF);
+        Snackbar sb = Snackbar.make(tBar, "Go to New York Time Article Search?", Snackbar.LENGTH_LONG)
+                .setAction("Yes", new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v){
+                        // activity need to be changed
+                        Intent nextHop = new Intent(Activity_newsfeed.this, MainActivity.class);
+                        startActivity(nextHop);
+                    }});
+        sb.show();
+    }
+
+    protected class NewsAdapter extends BaseAdapter
+    {
+        public NewsAdapter() {
+            super();
+        }
+
+        @Override
+        public int getCount() {
+            return news.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return news[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View oldView, ViewGroup parent)
+        {
+            LayoutInflater inflater = getLayoutInflater();
+
+            View newView = inflater.inflate(R.layout.activity_list_newsfeed,parent,false);
+
+            TextView rowText = (TextView) newView.findViewById(R.id.item);
+            //String textToShow = getItem(position).toString();
+            //rowText.setText(textToShow);
+            rowText.setText(getItem(position).toString());
+
+            return newView;
+        }
+    }
+}
