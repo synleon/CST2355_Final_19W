69a846bd8d2da475ff27990c96d958493d5b7c81
Belle <joijoij>
2019-04-03 16:40:00 -0400

    dict Milestone2 2nd addition commit v1.0 Apr.3th

--- app/src/main/java/com/example/cst2355_final_19w/Activity_dict.java
+++ app/src/main/java/com/example/cst2355_final_19w/Activity_dict.java
@@ -1,11 +1,17 @@
 package com.example.cst2355_final_19w;
 
+
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -14,7 +20,6 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class Activity_dict extends AppCompatActivity {
 
@@ -28,6 +33,11 @@ public class Activity_dict extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dict);
 
+        Toolbar bar = (Toolbar)findViewById(R.id.toolbar_dict);
+        setSupportActionBar(bar);
+
+        ListView theList = (ListView) findViewById(R.id.dict_list);
+
         Button btn = findViewById(R.id.dict_button);
         btn.setOnClickListener(v -> {
             Snackbar sb = Snackbar.make(btn, "Go back?", Snackbar.LENGTH_LONG)
@@ -38,28 +48,49 @@ public class Activity_dict extends AppCompatActivity {
         Toast.makeText(this, "Welcome to dictionary page!", Toast.LENGTH_LONG).show();
 
         ProgressBar progressBar = findViewById(R.id.dict_progressBar);
+        progressBar.setProgress(100);
 
         ListView listViewWordList = findViewById(R.id.dict_list);
-
         wordListAdapter = new WordListAdapter(this, R.id.dict_list);
-
         listViewWordList.setAdapter(wordListAdapter);
-
         wordListAdapter.add("paraphrase");
-
         wordListAdapter.add("ostensible");
-
         wordListAdapter.add("digress");
-
         wordListAdapter.add("uncanny");
-
-        progressBar.setProgress(100);
 
         // Setting the item click listener for the listview
         listViewWordList.setOnItemClickListener((parent, container, position, id) -> {
             String word = (String)parent.getItemAtPosition(position);
             WordInfoDialog(word);
         });
+
+        boolean isTablet = findViewById(R.id.fragmentLocation) != null;
+
+        //get a database:
+        Dict_MyDatabaseOpenHelper dbOpener = new Dict_MyDatabaseOpenHelper(this);
+        SQLiteDatabase db = dbOpener.getWritableDatabase();
+        db = dbOpener.getWritableDatabase();
+
+      /*  //query all the results from the database:
+        String[] columns = {Dict_MyDatabaseOpenHelper.COL_ID, Dict_MyDatabaseOpenHelper.COL_word};
+        results = db.query(false, Dict_MyDatabaseOpenHelper.TABLE_NAME, columns, null, null, null, null, null, null);
+
+        //find the column indices:
+        int wordColIndex = results.getColumnIndex(Dict_MyDatabaseOpenHelper.COL_word);
+        int idColIndex = results.getColumnIndex(Dict_MyDatabaseOpenHelper.COL_ID);
+
+        //iterate over the results, return true if there is a next item:
+        while (results.moveToNext()) {
+            String word = results.getString(wordColIndex);
+            long id = results.getLong(idColIndex);
+
+            //add the new word to the array list:
+            words.add(new Word(word, id));
+        }
+
+        ListAdapter adt = new ListAdapter();
+        theList.setAdapter(adt); */
+
     }
 
     /**
@@ -140,5 +171,65 @@ public class Activity_dict extends AppCompatActivity {
 
             return view;
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dict_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.help:
+                helpDialog();
+                return true;
+
+            case R.id.search:
+                searchDialog();
+                return true;
+
+            case R.id.choice4:
+                Toast.makeText(this, "You clicked on the overflow menu", Toast.LENGTH_LONG).show();
+
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void helpDialog() {
+        View helpView = getLayoutInflater().inflate(R.layout.dict_help, null);
+        TextView textViewIntro = helpView.findViewById(R.id.textView_intro);
+        TextView textViewInstruction = helpView.findViewById(R.id.instruction);
+
+        textViewIntro.setText("Dictionary.com which was invented by Disken in 1965, is the world’s leading online source for English definitions, synonyms, word origins,audio pronunciations, example sentences, slang phrases, idioms, word games, legal and medical terms, Word of the Day and more. V 5.5 is the latest version.");
+        textViewInstruction.setText("How to use the dictionary? 1. Browse the dictionary. 2. Searching the dictionary using the Quick Search. 3. Search the dictionary using the Advanced Search. 4. Categories. 5. Sources. 6. Historical Thesaurus. 7. Other resources.");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("OK", null)
+                .setNegativeButton("Cancel", null);
+
+        builder.create().show();
+    }
+
+    public void searchDialog() {
+        View helpView = getLayoutInflater().inflate(R.layout.dict_searchdialogue, null);
+
+        Button search = findViewById(R.id.dict_search);
+        search.setOnClickListener(v->{
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setPositiveButton("OK", null)
+                .setNegativeButton("Cancel", null);
+
+        builder.create().show();
+
+        });
+
+
     }
 }
